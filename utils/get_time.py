5f2eefe85aff6f274a6cefac59e81257b07ce057
YuzukiTsuru <740291272@qq.com>
2020-11-14 20:46:35 +0800

    add todo

--- utils/get_time.py
+++ utils/get_time.py
@@ -5,5 +5,5 @@ def get_time() -> str:
     return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
 
-def get_timestamp() -> int:
+def get_timestamp() -> float:
     return time.time()
