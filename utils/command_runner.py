ca21afeeb2e7a0e13b7b05ca6b96b875acca5ad6
YuzukiTsuru <740291272@qq.com>
2020-11-14 19:29:56 +0800

    bug fix command runner

--- utils/command_runner.py
+++ utils/command_runner.py
@@ -15,7 +15,7 @@ def command_runner(shell_cmd: str) -> int:
         line = str(line.strip())[2:len(line)]
         if line:
             print('{}: [{}]'.format(get_time(), line))
-            os.system('echo {} >> GCCBench_{}.log'.format(line, time.time()))
+            os.system('echo "{}" >> GCCBench_{}.log'.format(line, time.time()))
     if p.returncode is 0:
         print('Subprogram success')
         return 0
