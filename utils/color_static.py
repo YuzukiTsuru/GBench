138db04959b77e557edd98f552ab7f4021eb3726
YuzukiTsuru <740291272@qq.com>
2020-11-14 21:04:26 +0800

    download source file

--- utils/color_static.py
+++ utils/color_static.py
@@ -2,7 +2,7 @@ from colorama import Fore
 
 BUILD = Fore.YELLOW + "Build" + Fore.WHITE
 
-DOWNLOAD = Fore.CYAN + 'Download file' + Fore.WHITE
+DOWNLOAD = Fore.CYAN + 'Download Source file' + Fore.WHITE
 DECOMPRESS = Fore.CYAN + 'Decompress file' + Fore.WHITE
 CONFIGURE = Fore.CYAN + 'Configure Build Script' + Fore.WHITE
 CLEAN = Fore.CYAN + 'Clean last build file' + Fore.WHITE
