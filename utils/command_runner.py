a917b8b1e03096773c6f15f3b8ba40beaba936c3
YuzukiTsuru <740291272@qq.com>
2020-11-14 20:09:37 +0800

    delete log

--- utils/command_runner.py
+++ utils/command_runner.py
@@ -18,7 +18,8 @@ def command_runner(shell_cmd: str, flush=False) -> int:
         line = str(line.strip())[2:len(line) + 1]
         if line:
             if flush:
-                sys.stdout.write('\r' + strarrs[i % 3] + ' {}: [{}]\r'.format(get_time(), line))
+                sys.stdout.write(strarrs[i % 3] + ' {}: [{}]\r'.format(get_time(), line))
+                sys.stdout.write('                                                                                         ')
                 sys.stdout.flush()
                 i += 1
             else:
