af160feeb1d187d2e38bf59ff953b5fa1be2f248
YuzukiTsuru <740291272@qq.com>
2020-11-14 19:32:50 +0800

    bug fix log fix

--- utils/command_runner.py
+++ utils/command_runner.py
@@ -7,6 +7,7 @@ from .get_time import get_time
 
 
 def command_runner(shell_cmd: str) -> int:
+    log_time = time.time()
     cmd = shlex.split(shell_cmd)
     p = subprocess.Popen(
         cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -15,7 +16,7 @@ def command_runner(shell_cmd: str) -> int:
         line = str(line.strip())[2:len(line)]
         if line:
             print('{}: [{}]'.format(get_time(), line))
-            os.system('echo "{}" >> GCCBench_{}.log'.format(line, time.time()))
+            os.system('echo "{}" >> GCCBench_{}.log'.format(line, log_time))
     if p.returncode is 0:
         print('Subprogram success')
         return 0
