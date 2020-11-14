bbca6672d50540cea9dd17e92043de1c43137bf2
YuzukiTsuru <740291272@qq.com>
2020-11-14 19:12:43 +0800

    bug fix AttributeError: type object 'timer' has no attribute 'timer'

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import argparse
 import source.gcc as gcc
 import source.nginx as nginx
 
-import utils.timer as timer
+import utils.timer as timer_main
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="GCCBench v1.0")
     parser.add_argument('-n', '--name', default='gcc')
     args = parser.parse_args()
 
-    timer = timer.timer()
+    timer_main = timer_main.timer()
 
     if 'gcc' in args.name:
-        timer.timer_start()
+        timer_main.timer_start()
         gcc_build = gcc.gcc()
         gcc_build.build_code()
-        timer.timer_end()
+        timer_main.timer_end()
     elif 'nginx' in args.name:
-        timer.timer_start()
+        timer_main.timer_start()
         nginx_build = nginx.nginx()
         nginx_build.build_code()
-        timer.timer_end()
+        timer_main.timer_end()
     else:
         raise 'The test of {} is not yet supported'.format(args.name)
