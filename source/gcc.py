43da4195bfd0676fda5946feeb4c3434dab793de
柚木鉉 <740291272@qq.com>
2020-11-16 10:03:54 +0800

    Update gcc.py

--- source/gcc.py
+++ source/gcc.py
@@ -15,11 +15,11 @@ class gcc:
         if not os.path.exists('{}.tar.gz'.format(self.package_name)) or not os.path.exists(self.package_name):
             command_runner('wget https://mirrors.aliyun.com/gnu/gcc/{}/{}.tar.gz'.format(self.package_name, self.package_name), DOWNLOAD)
             command_runner('tar xvzf {}.tar.gz'.format(self.package_name), DECOMPRESS)
-        command_runner('./{}/configure'.format(self.package_name), CONFIGURE)
+        command_runner('./configure', CONFIGURE, cwd_dir=self.package_name)
 
     def build_code(self):
-        command_runner('make clean', CLEAN)
-        command_runner('make -j{}'.format(self.core_num), BUILD, thread_num=self.core_num)
+        command_runner('make clean', CLEAN, cwd_dir=self.package_name)
+        command_runner('make -j{}'.format(self.core_num), BUILD, thread_num=self.core_num, cwd_dir=self.package_name)
 
     def get_core_count(self) -> int:
         return self.core_num
