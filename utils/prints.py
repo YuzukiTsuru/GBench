e17926a8c22d79f71c4f47abf9f6ff898c60ca51
YuzukiTsuru <zyj201103>
2020-11-18 16:27:03 +0800

    tqdm bug fix

--- utils/prints.py
+++ utils/prints.py
@@ -4,15 +4,14 @@ from .get_time import get_time
 
 
 def color_thread_stdout(color_thread_str: str, print_str: str) -> str:
-    return '{} | {}'.format(color_thread_str, print_str)
+    return '{} | [{}]'.format(color_thread_str, print_str)
 
 
 class Prints:
     def __init__(self, stage):
-        self.tqdm = tqdm(bar_format='{postfix[0]} | {postfix[1]} | [{postfix[2]}]', postfix=[None, stage, None])
+        self.tqdm = tqdm(bar_format='{postfix[0]} | {postfix[1]} | {postfix[2]}', postfix=[None, stage, None])
 
     def prints(self, print_str: str, color_thread=None):
-        print_str.replace('\n', '')
         if color_thread is None:
             self.tqdm.postfix[0] = get_time()
             self.tqdm.postfix[2] = print_str
